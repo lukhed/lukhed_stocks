f5c5299da0acde9d340ffd67f202b841f2700e65
lukhed <lukhed.mail@gmail.com>
2025-12-23 16:33:27 -0500

    upped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lukhed_stocks",
-    version="0.3.0",
+    version="0.3.1",
     description="A collection of stock analysis functions and api wrappers",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
